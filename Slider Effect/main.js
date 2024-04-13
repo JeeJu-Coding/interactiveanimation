754e8b2faf14b509bb16e0a69cf636c6e6b13051
JeeJu-Coding <145584473+JeeJu-Coding@users.noreply.github.com>
2024-04-13 22:20:35 +0530

    Add files via upload

---
+++ Slider Effect/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($){
+
+    $(document).on('click', '.blue', function(){
+        $(this).toggleClass('active');
+        $('main').toggleClass('active');
+    });
+
+    $(document).on('click', '.red', function(){
+        $(this).toggleClass('active');
+        $('main').toggleClass('active');
+    });
+
+    $(document).on('click', '.yellow', function(){
+        $(this).toggleClass('active');
+        $('main').toggleClass('active');
+    });
+
+});
